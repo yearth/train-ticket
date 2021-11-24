0adc2391e65c9633e8c898289574cdcc6d63e952
yearthmain <yearthmain@gmail.com>
2021-11-24 23:02:48 +0800

    [feat]: 初探 render-component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Resizable extends Component {
+  state = {
+    size: [window.innerWidth, window.innerHeight]
+  };
+
+  onResize = () => {
+    this.setState({ size: [window.innerWidth, window.innerHeight] });
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.onResize);
+    document.title = this.state.size.join("x");
+  }
+
+  componentDidUpdate() {
+    document.title = this.state.size.join("x");
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.onResize);
+  }
+
+  render() {
+    return this.props.render(this.state.size);
+  }
+}
+
+class Foo extends Component {
+  render() {
+    console.log("size", this.props.size);
+    const [width, height] = this.props.size;
+
+    return (
+      <div>
+        {width}x{height}
+      </div>
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return <Resizable render={size => <Foo size={size} />} />;
+  }
 }
 
 export default App;
