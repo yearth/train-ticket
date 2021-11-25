45f5b8a3753a51a133d2fb797f8ad0fa4421c945
yearthmain <yearthmain@gmail.com>
2021-11-25 23:05:58 +0800

    [feat]: useMemo

--- src/App.js
+++ src/App.js
@@ -1,110 +1,25 @@
-import React, { useState, useEffect } from "react";
+import { useState, useMemo } from "react";
 
-const useResizable = () => {
-  const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
+function App() {
+  const [count, setCount] = useState(0);
 
-  const onResize = () => {
-    setSize([window.innerWidth, window.innerHeight]);
-  };
+  const double = useMemo(() => {
+    return count * 2;
+  }, [count]);
 
-  useEffect(() => {
-    window.addEventListener("resize", onResize);
-    return () => {
-      window.removeEventListener("resize", onResize);
-    };
-  });
+  const sixAndEight = useMemo(() => {
+    return count * 2;
+  }, [count === 3]);
 
-  return [size];
-};
-
-const Foo = () => {
-  const [size] = useResizable();
-
-  useEffect(() => {
-    document.title = size.join("x");
-  });
-
-  return <div>{size.join("x")}</div>;
-};
-
-// const resizableHOC = Child => {
-//   return class Wrapper extends Component {
-//     state = {
-//       size: [window.innerWidth, window.innerHeight]
-//     };
-
-//     onResize = () => {
-//       this.setState({ size: [window.innerWidth, window.innerHeight] });
-//     };
-
-//     componentDidMount() {
-//       window.addEventListener("resize", this.onResize);
-//       document.title = this.state.size.join("x");
-//     }
-
-//     componentDidUpdate() {
-//       document.title = this.state.size.join("x");
-//     }
-
-//     componentWillUnmount() {
-//       window.removeEventListener("resize", this.onResize);
-//     }
-
-//     render() {
-//       const size = this.state.size;
-//       return <Child size={size} />;
-//     }
-//   };
-// };
-
-// class Resizable extends Component {
-//   state = {
-//     size: [window.innerWidth, window.innerHeight]
-//   };
-
-//   onResize = () => {
-//     this.setState({ size: [window.innerWidth, window.innerHeight] });
-//   };
-
-//   componentDidMount() {
-//     window.addEventListener("resize", this.onResize);
-//     document.title = this.state.size.join("x");
-//   }
-
-//   componentDidUpdate() {
-//     document.title = this.state.size.join("x");
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener("resize", this.onResize);
-//   }
-
-//   render() {
-//     return this.props.render(this.state.size);
-//   }
-// }
-
-// class Foo extends Component {
-//   render() {
-//     const [width, height] = this.props.size;
-//     return (
-//       <div>
-//         {width}x{height}
-//       </div>
-//     );
-//   }
-// }
-
-// const WrapperedFoo = resizableHOC(Foo);
-
-// class App extends Component {
-//   render() {
-//     // return <Resizable render={size => <Foo size={size} />} />;
-//     return <WrapperedFoo />;
-//   }
-// }
-function App(props) {
-  return <Foo />;
+  return (
+    <button
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    >
+      count:{count}, double:{double} sixAndEight:{sixAndEight}
+    </button>
+  );
 }
 
 export default App;
