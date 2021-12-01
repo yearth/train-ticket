8284c742f040d6c898a33b532c1a0f1ea2de681f
yearthmain <yearthmain@gmail.com>
2021-12-01 23:30:01 +0800

    [feat]: 创建组件骨架

--- src/home/App.js
+++ src/home/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
+import Header from "../common/components/Header";
+import DepartDate from "./components/DepartDate";
+import HighSpeed from "./components/HighSpeed";
+import Journey from "./components/Journey";
+import Submit from "./components/Submit";
 
 function App() {
-  return <div></div>;
+  return (
+    <main>
+      <Header />
+      <Journey />
+      <DepartDate />
+      <HighSpeed />
+      <Submit />
+    </main>
+  );
 }
 
 export default connect(
