74d1423e21ad1c1f2a260c00bdba50c7dea8050d
yearthmain <yearthmain@gmail.com>
2021-12-01 22:28:56 +0800

    [feat]: 将项目改造成 mpa

---
+++ src/home/reducers.js
@@ -0,0 +1,2 @@
+const reducer = {};
+export default reducer;
---
+++ src/order/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { connect } from "react-redux";
+
+function App() {
+  return <div></div>;
+}
+
+export default connect(
+  // mapStateToProps
+  state => {},
+  // mapDispatchToProps
+  dispatch => {}
+)(App);
