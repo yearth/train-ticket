823697763f7aad7b4eea9e569c46f07faef55f4f
yearthmain <yearthmain@gmail.com>
2021-12-02 00:05:39 +0800

    [feat]: 创建 reducer 骨架

--- src/home/reducers.js
+++ src/home/reducers.js
@@ -1,2 +1,87 @@
-const reducer = {};
+import {
+  ACTION_SET_FROM,
+  ACTION_SET_TO,
+  ACTION_SET_IS_CITY_SELECTOR_VISIBLE,
+  ACTION_SET_CURRENT_SELECTING_LEFT_CITY,
+  ACTION_SET_CIDY_DATA,
+  ACTION_SET_IS_LOADING_CITY_DATA,
+  ACTION_SET_IS_DATE_SELECTOR_VISIBLE,
+  ACTION_SET_HIGH_SPEED
+} from "./actions";
+
+const reducer = {
+  from(state = "北京", action) {
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_FROM:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  to(state = "上海", action) {
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_TO:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  isCitySelectorVisible(state = false, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_IS_CITY_SELECTOR_VISIBLE:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  currentSelectingLeftCity(state = false, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_CURRENT_SELECTING_LEFT_CITY:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  cidyData(state = null, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_CIDY_DATA:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  isLoadingCityData(state = false, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_IS_LOADING_CITY_DATA:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  isDateSelectorVisible(state = false, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_IS_DATE_SELECTOR_VISIBLE:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  highSpeed(state = false, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_HIGH_SPEED:
+        return payload;
+      default:
+    }
+    return state;
+  }
+};
+
 export default reducer;
