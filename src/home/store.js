d319e814524b147819641afd2f440ed41a7a6e72
yearthmain <yearthmain@gmail.com>
2021-12-01 23:34:19 +0800

    [feat]: 创建 state 骨架

--- src/home/store.js
+++ src/home/store.js
@@ -2,6 +2,19 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
 
-const store = createStore(combineReducers(reducers), {}, applyMiddleware(thunk));
+const store = createStore(
+  combineReducers(reducers),
+  {
+    from: "北京",
+    to: "上海",
+    isCitySelectorVisible: false,
+    currentSelectingLeftCity: false,
+    cidyData: null,
+    isLoadingCityData: false,
+    isDateSelectorVisible: false,
+    highSpeed: false
+  },
+  applyMiddleware(thunk)
+);
 
 export default store;
