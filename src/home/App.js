65c8b71baa9672d26ac03313b85b00abdef56d5b
yearthmain <yearthmain@gmail.com>
2021-12-06 22:36:03 +0800

    [feat]: 顶部导航栏

--- src/home/App.js
+++ src/home/App.js
@@ -1,23 +1,53 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import Header from "../common/components/Header";
 import DepartDate from "./components/DepartDate";
 import HighSpeed from "./components/HighSpeed";
 import Journey from "./components/Journey";
 import Submit from "./components/Submit";
+import { showCitySelector, exchangeFromTo } from "./actions";
 
-function App() {
+function App(props) {
+  const { from, to, dispatch } = props;
+
   const onBack = useCallback(() => {
     window.history.back();
+  }, []);
+
+  // const doExchangeFromTo = useCallback(() => {
+  // dispatch(exchangeFromTo());
+  // }, []);
+  //
+  // const doShowCitySelector = useCallback(m => {
+  // dispatch(showCitySelector(m));
+  // }, []);
+
+  const dispatchs = useMemo(() => {
+    return bindActionCreators(
+      {
+        exchangeFromTo,
+        showCitySelector
+      },
+      dispatch
+    );
   }, []);
 
   return (
     <main>
       <Header title="火车票" onBack={onBack} />
-      <Journey />
-      <DepartDate />
-      <HighSpeed />
-      <Submit />
+      <form className="form">
+        <Journey
+          from={from}
+          to={to}
+          {...dispatchs}
+          // exchangeFromTo={doExchangeFromTo}
+          // showCitySelector={doShowCitySelector}
+        />
+        <DepartDate />
+        <HighSpeed />
+        <Submit />
+      </form>
     </main>
   );
 }
@@ -25,10 +55,10 @@ function App() {
 export default connect(
   // mapStateToProps
   state => {
-    return {};
+    return state;
   },
   // mapDispatchToProps
   dispatch => {
-    return {};
+    return { dispatch };
   }
 )(App);
--- src/home/components/Journey/index.js
+++ src/home/components/Journey/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import "./index.css";
 
-export default function Journey() {
-  return <div></div>;
+export default function Journey(props) {
+  const { from, to, showCitySelector, exchangeFromTo } = props;
+  return (
+    <div className="journey">
+      <div className="journey-station" onClick={() => showCitySelector(true)}>
+        <input
+          type="text"
+          readOnly
+          name="from"
+          value={from}
+          className="journey-input journey-from"
+        />
+      </div>
+      <div className="journey-switch" onClick={() => exchangeFromTo()}>
+        x
+      </div>
+      <div className="journey-station" onClick={() => showCitySelector(false)}>
+        <input type="text" readOnly name="to" value={to} className="journey-input journey-to" />
+      </div>
+    </div>
+  );
 }
