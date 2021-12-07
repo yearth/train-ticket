c59116a4a4a712fefe165556ee9a3d4710e93c11
yearthmain <yearthmain@gmail.com>
2021-12-07 23:21:07 +0800

    [feat]: 城市选择浮层组件

--- src/common/components/CitySelector/index.js
+++ src/common/components/CitySelector/index.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import classnames from "classnames";
 import "./index.css";
 
 export default function CitySelector(props) {
   const { show, loading, data } = props;
+
+  const [value, setValue] = useState("");
 
   return (
     <div
@@ -16,6 +18,23 @@ export default function CitySelector(props) {
           <svg width="42" height="42">
             <polyline points="25,13 16,21 25,29" stroke="#fff" strokeWidth="2" fill="none" />
           </svg>
+        </div>
+
+        <div className="search-input-wrapper">
+          <input
+            value={value}
+            type="text"
+            className="search-input"
+            placeholder="城市、车站的中文或拼音"
+            onChange={e => setValue(e.target.value)}
+          />
+          <span
+            className={classnames("search-clean", {
+              hidden: value.length === 0
+            })}
+          >
+            x
+          </span>
         </div>
       </div>
     </div>
