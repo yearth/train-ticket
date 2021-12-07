9839477807f88d8e4b018138033ad4c02f17d339
yearthmain <yearthmain@gmail.com>
2021-12-07 23:08:49 +0800

    [feat]: 城市选择浮层组件

--- src/common/components/CitySelector/index.js
+++ src/common/components/CitySelector/index.js
@@ -1,8 +1,23 @@
 import React from "react";
+import classnames from "classnames";
 import "./index.css";
 
 export default function CitySelector(props) {
   const { show, loading, data } = props;
 
-  return <div className={["city-selector", !show && "hidden"].filter(Boolean).join(" ")}></div>;
+  return (
+    <div
+      className={classnames("city-selector", {
+        hidden: !show
+      })}
+    >
+      <div className="city-search">
+        <div className="city-back">
+          <svg width="42" height="42">
+            <polyline points="25,13 16,21 25,29" stroke="#fff" strokeWidth="2" fill="none" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
 }
